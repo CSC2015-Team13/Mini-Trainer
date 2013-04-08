32d98812bdd42773d6bcbdc90841273b2103fa43
Viktor Movcan <viktor.movcan@gmail.com>
2013-04-08 17:58:27 +0100

    the newest version. ignore 0.5m
    same updates as in 0.5m

---
+++ App/0.5p/src/com/minitrainer/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.minitrainer;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SettingsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+	}
+}
